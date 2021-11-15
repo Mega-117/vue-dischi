b6e13339d0ced7acff2f26e82c00925150345dc0
Mega-117 <enricosaian@hotmail.it>
2021-11-15 14:26:19 +0100

    modifiche per il push

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     node: true
   },
+  ignorePatterns: ["dist/*"],
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended'
@@ -14,4 +15,4 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-}
+};
